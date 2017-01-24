0d25031d44e89f827c40b35b302fbeb8cf7a17cd
Petra Stone <petracstone@gmail.com>
2017-01-24 16:59:12 +0000

    Cache Matrix Script

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To demonstrate Lexical Scoping in R, these functions cache
+## inversion of a user-defined matrix so that an expensive operation does not
+## need to be performed repeatedly.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes an input of a matrix
+## It has getters and setters for updating or setting matrix data
+## It also initialises the inverse setters and getters so they can be used
+## in the other function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        # Assign the input argument to the x object in the parent environment
+        x <<- y
+        # Assign the value of NULL to the i object in the parent environment
+        i <<- NULL
+    }
+    get <- function() x # Retrieves x from the parent environment
+    setinvm <- function(invm) i <<- invm #  i in the parent environment
+    getinvm <- function() i # Retrieves i from the parent environment
+    
+    # Return list of functions to parent environment
+    list(set = set, get = get,
+         setinvm = setinvm,
+         getinvm = getinvm)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve requires an input of type makeCacheMatrix()
+## If the makeCacheMatrix object has already been stored it uses
+## the cached version of it, otherwise it calculates the inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Requires an input argument of type makeCacheMatrix()
+    i <- x$getinvm()
+    if(!is.null(i)) {
+        message("getting cached inverse matrix")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinvm(i)
+    return(i)
 }
+
+# # Tests
+# set.seed(9801)
+# x = matrix(rexp(35*35), 35, 35)
+# mat = makeCacheMatrix(x)
+# 
+# # First run, not cached
+# head(cacheSolve(mat), 2)
+# 
+# # Second run cached
+# head(cacheSolve(mat), 2)
